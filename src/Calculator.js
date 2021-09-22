d819e00009116c69f1c0bf214193d68b3886e570
nathanieldcooke <nathanield.cooke@gmail.com>
2021-09-22 07:12:47 -0700

    complete phase 5

--- src/Calculator.js
+++ src/Calculator.js
@@ -16,14 +16,49 @@ class Calculator extends React.Component {
         this.setState({num1: num});
     }
 
+    handleSecondNum = e => {
+        const num = Number(e.target.value) ? Number(e.target.value) : "";
+        this.setState({ num2: num });
+    }
+
+    sum = () => {
+        this.setState({ result: this.state.num1 + this.state.num2 });
+    }
+
+    subtract = () => {
+        this.setState({ result: this.state.num1 - this.state.num2 });
+    }
+
+    multiply = () => {
+        this.setState({ result: this.state.num1 * this.state.num2 });
+    }
+
+    divide = () => {
+        this.setState({ result: this.state.num1 / this.state.num2 });
+    }
+
+    clearInput = () => {
+        this.setState({
+            result: 0, 
+            num1: '', 
+            num2: ''
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>Result: {this.state.result}</h1>
                 <input type='number' value={this.state.num1} onChange={this.handleFirstNum} placeholder="#" />
+                <input type='number' value={this.state.num2} onChange={this.handleSecondNum} placeholder="#" />
+                <button onClick={this.sum} >+</button>
+                <button onClick={this.subtract} >-</button>
+                <button onClick={this.multiply} >*</button>
+                <button onClick={this.divide} >/</button>
+                <button onClick={this.clearInput} >clear</button>
             </div>
-        )
-    }
-}
+        );
+    };
+};
 
 export default Calculator
